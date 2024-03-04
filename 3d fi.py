6e0466961a7c80f7d26098b7e51ddc49d7ad556f
Anuwilfred <156174065+Anuwilfred@users.noreply.github.com>
2024-03-04 16:59:28 +0400

    Add files via upload

---
+++ 3d fi.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.tri import Triangulation
+ 
+def f(x, y):
+    return np.sin(np.sqrt(x ** 2 + y ** 2))
+ 
+x = np.linspace(-6, 6, 30)
+y = np.linspace(-6, 6, 30)
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+ 
+tri = Triangulation(X.ravel(), Y.ravel())
+ 
+fig = plt.figure(figsize=(10, 5))
+ax = fig.add_subplot(111, projection='3d')
+ 
+ax.plot_trisurf(tri, Z.ravel(), cmap='cool', edgecolor='none', alpha=0.2)
+ 
+ax.set_title('Surface Triangulation Plot of f(x, y) =\
+                sin(sqrt(x^2 + y^2))', fontsize=14)
+ax.set_xlabel('x', fontsize=12)
+ax.set_ylabel('y', fontsize=12)
+ax.set_zlabel('z', fontsize=12)
+ 
+plt.show()
